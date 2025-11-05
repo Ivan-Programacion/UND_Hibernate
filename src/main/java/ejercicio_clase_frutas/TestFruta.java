e1713e14d65b1f6adb7c5b3b4212e1261aeeb253
Iván Vázquez Martín <ivanvmartin@gmail.com>
2025-11-05 17:28:47 +0100

    Ejercicio fruta terminado

--- src/main/java/ejercicio_clase_frutas/TestFruta.java
+++ src/main/java/ejercicio_clase_frutas/TestFruta.java
@@ -21,23 +21,28 @@ public class TestFruta {
 		System.out.println("Saliendo...");
 		miSesion.close();
 		sessionFactory.close();
+		sc.close();
 	}
 
 	private static void menuFrutas(Scanner sc, Session miSesion) {
 		int opcionUsuario = 0;
-		while (opcionUsuario != 5) {
-			listarFrutas(miSesion);
-			System.out.println();
+		while (opcionUsuario != 6) {
+			if (opcionUsuario != 2) {
+				listarFrutas(miSesion);
+				System.out.println();
+			}
 			System.out.println("Selecciona una opcion (escribe el número correspondiente):");
 			System.out.println("1. Añadir fruta");
 			System.out.println("2. Ver lista de frutas");
-//			System.out.println("3. Eliminar fruta");
-//			System.out.println("4. Vaciar lista de frutas");
-			System.out.println("5. Salir");
+			System.out.println("3. Modificar fruta");
+			System.out.println("4. Eliminar fruta");
+			System.out.println("5. Vaciar lista de frutas");
+			System.out.println("6. Salir");
 			opcionUsuario = sc.nextInt();
 			System.out.println();
 			opciones(sc, opcionUsuario, miSesion);
-			System.out.println();
+			if (opcionUsuario != 6)
+				System.out.println();
 		}
 	}
 
@@ -49,18 +54,61 @@ public class TestFruta {
 		else if (opcionUsuario == 2)
 			listarFrutas(miSesion);
 		else if (opcionUsuario == 3)
-			eliminarFruta(sc, miSesion);
+			modificarDatos(sc, miSesion);
 		else if (opcionUsuario == 4)
-			eliminarListaFruta(miSesion);
+			eliminarFruta(sc, miSesion);
+		else if (opcionUsuario == 5)
+			eliminarListaFruta(sc, miSesion);
 		transaction.commit();
 	}
 
-	private static void eliminarListaFruta(Session miSesion) {
+	private static void modificarDatos(Scanner sc, Session miSesion) {
+		Long id = 0L;
+		System.out.println("Escribe el ID de la fruta que quieres modificar:");
+		id = sc.nextLong();
+		Fruta frutaSeleccionada = miSesion.find(Fruta.class, id);
+		if (frutaSeleccionada != null) {
+			System.out.println("Fruta seleccionada:");
+			System.out.printf("ID: %d | Fruta: %s | Peso: %d | Número color: %d | Estado: %s",
+					frutaSeleccionada.getId(), frutaSeleccionada.getNombre(), frutaSeleccionada.getPeso(),
+					frutaSeleccionada.getColor(), frutaSeleccionada.isBuenAspecto() ? "Buen estado" : "Mal estado");
+			System.out.println("Cambiar datos:");
+			System.out.println("Fruta:");
+			frutaSeleccionada.setNombre(sc.next());
+			System.out.println("Peso:");
+			frutaSeleccionada.setPeso(sc.nextInt());
+			System.out.println("Número de color:");
+			frutaSeleccionada.setColor(sc.nextInt());
+			System.out.println("Tiene buen aspecto? (Seleccione una opción)");
+			System.out.println("1. Sí");
+			System.out.println("2. No");
+			int respuesta = sc.nextInt();
+			frutaSeleccionada.setBuenAspecto(respuesta == 1 ? true : false);
+			System.out.println("Modificando fruta de la lista...");
+			miSesion.merge(frutaSeleccionada);
+			System.out.println("Fruta modificada con éxtio");
+		}
+	}
 
+	private static void eliminarListaFruta(Scanner sc, Session miSesion) {
+		int respuesta = 0;
+		List<Fruta> lista = miSesion.createQuery("from Fruta", Fruta.class).list();
+		System.out.println("Estás seguro de que queires eliminar toda la lista?");
+		System.out.println("1. Sí");
+		System.out.println("2. No");
+		respuesta = sc.nextInt();
+		if (respuesta == 1) {
+			System.out.println("Eliminando lista entera...");
+			for (Fruta fruta : lista)
+				miSesion.remove(fruta);
+			System.out.println("Lista eliminada");
+		} else
+			System.out.println("Volviendo al menú...");
 	}
 
 	private static void eliminarFruta(Scanner sc, Session miSesion) {
 		String respuesta = "";
+		Long id = 0L;
 		System.out.println("Escribe la fruta que quieras eliminar:");
 		respuesta = sc.next();
 		List<Fruta> listaFrutas = miSesion.createQuery("from Fruta", Fruta.class).list();
@@ -69,24 +117,34 @@ public class TestFruta {
 			if (fruta.getNombre().equalsIgnoreCase(respuesta))
 				listaFrutasEncontrada.add(fruta);
 		if (listaFrutasEncontrada.size() > 1) {
+			System.out.println("Fruta " + respuesta + " encontradas:");
 			for (Fruta fruta : listaFrutasEncontrada)
-				System.out.println(fruta.getId() + " - " + fruta.getPeso() + " - " + fruta.getColor() + " - "
+				System.out.println("ID: " + fruta.getId() + " | " + "Peso: " + fruta.getPeso() + " | "
+						+ "Número color: " + fruta.getColor() + " | " + "Estado: "
 						+ (fruta.isBuenAspecto() ? "Buen estado" : "Mal estado"));
-		} else {
-
-		}
+			System.out.println("Escribe el id de quien quieras eliminar:");
+			id = sc.nextLong();
+		} else
+			for (Fruta fruta : listaFrutasEncontrada)
+				id = fruta.getId();
+		System.out.println("Eliminando " + respuesta + " con ID " + id + "...");
+		for (Fruta fruta : listaFrutasEncontrada)
+			if (fruta.getId() == id)
+				miSesion.remove(fruta);
+		System.out.println("Fruta eliminada");
 
 	}
 
 	private static void listarFrutas(Session miSesion) {
 		System.out.println("LISTA FRUTAS");
 		List<Fruta> listaFrutas = miSesion.createQuery("from Fruta", Fruta.class).list();
-		if (listaFrutas.isEmpty())
+		if (listaFrutas.isEmpty()) {
 			System.out.println("(vacío)");
-		else {
+		} else {
 			for (Fruta fruta : listaFrutas) {
-				System.out.printf("Fruta: %s | Peso: %d | Número color: %d | Estado: %s", fruta.getNombre(),
-						fruta.getPeso(), fruta.getColor(), fruta.isBuenAspecto() ? "Buen estado" : "Mal estado");
+				System.out.printf("ID: %d | Fruta: %s | Peso: %d | Número color: %d | Estado: %s", fruta.getId(),
+						fruta.getNombre(), fruta.getPeso(), fruta.getColor(),
+						fruta.isBuenAspecto() ? "Buen estado" : "Mal estado");
 				System.out.println();
 			}
 		}
